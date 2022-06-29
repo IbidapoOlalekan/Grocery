5788d5f67a348f7c07a2bd0cf93769104d779ed4
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-06-29 16:36:49 +0100

    Profile Screen  Commit

--- app/src/main/java/com/example/grocery/profiles/screens/Profile.kt
+++ app/src/main/java/com/example/grocery/profiles/screens/Profile.kt
@@ -1,4 +1,20 @@
 package com.example.grocery.profiles.screens
 
-class Profile {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Profile() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Profile")
+    }
 }
