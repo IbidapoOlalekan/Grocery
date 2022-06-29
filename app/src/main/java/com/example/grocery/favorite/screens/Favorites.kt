985408dc5af47a3111dbcad8bbf0cec284651406
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-06-29 16:36:13 +0100

    Favorite Screen  Commit

--- app/src/main/java/com/example/grocery/favorite/screens/Favorites.kt
+++ app/src/main/java/com/example/grocery/favorite/screens/Favorites.kt
@@ -1,2 +1,20 @@
 package com.example.grocery.favorite.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Favorites(){
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Favorites")
+    }
+}
